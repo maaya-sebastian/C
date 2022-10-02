aa6309cd56a32bb9616e6117c4f57de890fe8f7e
Maaya Sebastian <102866839+maaya-sebastian@users.noreply.github.com>
2022-10-02 22:00:22 +0530

    simple c program without using main function

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#define start main
+void start()
+{
+	printf("hello !");
+}
+
+/*(in linux terminal)
+for compilation : cc hello.c
+for execution : ./a.out
+
+OUTPUT :
+hello !
+*/
