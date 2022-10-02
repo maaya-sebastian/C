38bc4bf0a6c22d7e0198ff838c37052f227c0425
Maaya Sebastian <102866839+maaya-sebastian@users.noreply.github.com>
2022-10-02 21:42:39 +0530

    hello.c
    How to write a simple "Hello world" program in c without using the "main" function.

---
+++ hello.c
@@ -0,0 +1,24 @@
+// Printing a simple "HELLO WORLD" program without using "main" function.
+
+#include<stdio.h>
+#define start main
+void start()
+{
+	printf("hello !");
+}
+
+
+
+
+
+
+/*------------- NOTE :
+in linux: (in terminal)
+for compiling: cc hello.c
+for execution : ./a.out
+
+
+/*
+OUTPUT :
+hello !
+*/
